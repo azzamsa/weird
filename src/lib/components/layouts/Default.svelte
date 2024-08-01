14dfe14432109adbdff013af81d577295915dcba
azzamsa <me@azzamsa.com>
2024-08-01 20:38:11 +0700

    feat: new front page

---
+++ src/lib/components/layouts/Default.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { env } from '$env/dynamic/public';
+	import NavBar from '$lib/components/layouts/Navbar.svelte';
+</script>
+
+<svelte:head>
+	<title>{env.PUBLIC_INSTANCE_NAME}</title>
+</svelte:head>
+
+<NavBar />
+
+<div>
+	<slot></slot>
+</div>
